40922d396da8704f66f9c252fd2464ea5d61f3aa
dpjagustin <dpj.agustin@gmail.com>
2022-09-21 12:19:30 -0300

    Hola carola

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,9 @@ export default function Footer() {
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      color={useColorModeValue('gray.700', 'gray.200')}
+      mt={"12rem"}
+      >
       <Container as={Stack} maxW={'6xl'} py={10}>
         <SimpleGrid
           templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
