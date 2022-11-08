cb508d21c22dd291e8ae45f2a6af074347f58b99
dpjagustin <dpj.agustin@gmail.com>
2022-11-08 13:03:03 -0300

    Slider empresas

---
+++ src/components/SliderLanding.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/SliderLanding.css";
+
+export default function SliderLanding() {
+
+  return(
+    <>
+      <h2 className="tituloClientes">Confían en nosotros</h2>
+      <div className="wrapper">
+        <div className="slider">
+          <div className="slide">
+            <img src="./logoScorza.png" alt="" />
+            <img src="./logoPromedon.png" alt="" />
+            <img src="./logoIndumix.png" alt="" />
+            <img src="./logoVulcano.png" alt="" />
+            <img src="./logoCostantini.png" alt="" />
+            <img src="./logoCONAE.png" alt="" />
+            <img src="./logoVolkswagen.png" alt="" />
+            <img src="./logoAxion.jpg" alt="" />
+            <img src="./logoProdismo.jpg" alt="" />
+            <img src="./logoAspro.png" alt="" />
+            <img src="./logFelsim.jpg" alt="" />
+            <img src="./logoRichiger.png" alt="" />
+            <img src="./logFimaco.png" alt="" />
+            <img src="./logoFelsim.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import NavBarLanding from "../components/NavBarLanding";
 import HeroLanding from "../components/HeroLanding";
+import SliderLanding from "../components/SliderLanding";
 
 export default function Landing() {
   return(
     <>
       <NavBarLanding />
       <HeroLanding />
+      <SliderLanding />
       <Footer />
     </>
   )
